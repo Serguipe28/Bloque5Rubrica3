364e4925a3371f8d6ccc59e38282e95d12ea1dbb
Serguipe28 <serguipe28@gmail.com>
2022-02-01 01:08:58 +0100

    Creando codigo

---
+++ src/main/java/com/techniclaSerguipe/Spring4Example/Spring4ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techniclaSerguipe.Spring4Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4ExampleApplication.class, args);
+	}
+
+}
